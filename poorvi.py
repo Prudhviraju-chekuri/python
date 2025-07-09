32551b1ff5fa598496f67d7ea5d83d92485148eb
prudhviraju-chekuri <ch.prudhviraju@gmail.com>
2025-07-09 22:11:18 +0530

    Poorvi_Python_Traing

---
+++ poorvi.py
@@ -0,0 +1,31 @@
+# numbers --> integers ---> 0 1 2 3
+# decimal numbers--> floating numbers--> 1.3 3.1456
+# string ---->  Poorvi1 prudhvi2
+# boolean ---> IS_poorvi_Good_Girl
+
+a = 10.5
+b = 0.5
+print("A value is",a)
+print("sum of a and b is", a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+name = "Poorvika"
+print(name)
+
+
+is_poorvi_good_girl = True
+is_pari_badgirl = False
+
+print(is_poorvi_good_girl)
+print(is_pari_badgirl)
+
+msg1 = "Python "
+msg2 = "is "
+msg3 = "Awsome"
+
+print(msg1)
+print(msg2)
+print(msg3)
+print(msg1+msg2+msg3)
